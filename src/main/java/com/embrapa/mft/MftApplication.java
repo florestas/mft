c5c5fa35193ad8e00b6ecdd6d5ecaee97ea472f2
Carlos Melo <carlosmeloti@gmail.com>
2022-04-30 20:51:02 -0300

    primeiro commit

---
+++ src/main/java/com/embrapa/mft/MftApplication.java
@@ -0,0 +1,13 @@
+package com.embrapa.mft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/embrapa/mft/MftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embrapa.mft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
